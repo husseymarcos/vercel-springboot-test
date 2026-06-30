118a2fd9861e575904d7eb9653a0efc26baa2af7
husseymarcos <mhussey@mail.austral.edu.ar>
2026-06-30 11:46:59 -0300

    Update HelloController.java

--- src/main/java/com/example/vercel_springboot_test/HelloController.java
+++ src/main/java/com/example/vercel_springboot_test/HelloController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 class HelloController {
-    @GetMapping("/hello")
+    @GetMapping("/")
     String hello() {
         return "Hello from Spring Boot on Vercel";
     }
